6052f1505dee40f27d8d2d93f08e3d44c57a088d
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-11-09 23:27:28 +0800

    add pun feature

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const { SystemProgram } = web3
 const arr = Object.values(kp._keypair.secretKey)
 const secret = new Uint8Array(arr)
 const baseAccount = web3.Keypair.fromSecretKey(secret)
-
+console.log("base account", baseAccount)
 // Get our program's id form the IDL file.
 const programID = new PublicKey(idl.metadata.address)
 
@@ -36,10 +36,22 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 // 	"https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif",
 // ]
 
+const TEST_GIFS = [
+	{
+		gifLink: "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+		gifPun: "boob bloob boob",
+	},
+	{
+		gifLink: "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif",
+		gifPun: "stay pawstive",
+	},
+]
+
 const App = () => {
 	// State
 	const [walletAddress, setWalletAddress] = useState(null)
-	const [inputValue, setInputValue] = useState("")
+	const [linkValue, setlinkValue] = useState("")
+	const [punValue, setPunValue] = useState("")
 	const [gifList, setGifList] = useState([])
 
 	/*
@@ -115,22 +127,35 @@ const App = () => {
 		}
 		// Otherwise, we're good! Account exists. User can submit GIFs.
 		else {
+			console.log("gifs: ", gifList)
 			return (
 				<div className="connected-container">
-					<input
-						type="text"
-						placeholder="Enter gif link!"
-						value={inputValue}
-						onChange={onInputChange}
-					/>
-					<button className="cta-button submit-gif-button" onClick={sendGif}>
-						Submit
-					</button>
+					<div className="submit-container">
+						<input
+							className="submit1"
+							type="text"
+							placeholder="Enter gif link!"
+							value={linkValue}
+							onChange={onLinkInputChange}
+						/>
+						<input
+							className="submit2"
+							type="text"
+							placeholder="Insert pun here!"
+							value={punValue}
+							onChange={onPunInputChange}
+						/>
+						<button className="cta-button submit-gif-button submit3" onClick={sendGif}>
+							Submit
+						</button>
+					</div>
+
 					<div className="gif-grid">
 						{/* We use index as the key instead, also, the src is now item.gifLink */}
 						{gifList.map((item, index) => (
 							<div className="gif-item" key={index}>
 								<img src={item.gifLink} alt="" />
+								<p>{item.gifPun}</p>
 							</div>
 						))}
 					</div>
@@ -139,27 +164,39 @@ const App = () => {
 		}
 	}
 
-	const onInputChange = (event) => {
+	const onLinkInputChange = (event) => {
 		const { value } = event.target
-		setInputValue(value)
+		setlinkValue(value)
+	}
+
+	const onPunInputChange = (event) =>{
+		const {value} =event.target
+		setPunValue(value)
 	}
 
 	const sendGif = async () => {
-		if (inputValue.length === 0) {
+		if (linkValue.length === 0) {
 			console.log("No gif link given!")
 			return
+		} 
+
+		if (punValue.length === 0) {
+			console.log("No pun given")
 		}
-		console.log("Gif link:", inputValue)
+		
+		console.log("Gif link: ", linkValue)
+		console.log("Gif pun: ", punValue)
+
 		try {
 			const provider = getProvider()
 			const program = new Program(idl, programID, provider)
 
-			await program.rpc.addGif(inputValue, {
+			await program.rpc.addGif(linkValue, punValue, {
 				accounts: {
 					baseAccount: baseAccount.publicKey,
 				},
 			})
-			console.log("GIF sucesfully sent to program", inputValue)
+			console.log("GIF sucesfully sent to program", linkValue)
 
 			await getGifList()
 		} catch (error) {
@@ -209,7 +246,6 @@ const App = () => {
 				baseAccount.publicKey
 			)
 
-			console.log("Got the account", account)
 			setGifList(account.gifList)
 		} catch (error) {
 			console.log("Error in getGifs: ", error)
@@ -240,9 +276,9 @@ const App = () => {
 		<div className="App">
 			<div className={walletAddress ? "authed-container" : "container"}>
 				<div className="header-container">
-					<p className="header">🌳 TREE PORTAL</p>
+					<p className="header">🐋 WHALE HELLO THERE!</p>
 					<p className="sub-text">
-						View your GIF collection in the metaverse ✨
+						View the greatest animal pun collection in the metaverse.
 					</p>
 					{!walletAddress && renderNotConnectedContainer()}
 					{walletAddress && renderConnectedContainer()}
